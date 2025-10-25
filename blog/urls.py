d8d63e4e640ef90aeffdad79e3e02d7b0d8c15a2
jeremycabrera1 <jeremycabrera1000@gmail.com>
2025-10-24 23:40:49 -0400

    finished the update and delete CBVs they are much easier than FBVs

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import PostListView, PostCreateView
+from .views import PostListView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='posts'),
-    path('create-post', PostCreateView.as_view(), name='create_post'),
+    path('create-post/', PostCreateView.as_view(), name='create_post'),
+    path('<int:pk>/update-post/', PostUpdateView.as_view(), name='update_post'),
+    path('<int:pk>/delete-post/', PostDeleteView.as_view(), name='delete_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,34 @@
 from django.shortcuts import render, get_list_or_404
 from django.urls import reverse_lazy
 
-from django.views.generic import TemplateView, ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 # Create your views here.
 
 from .models import Category, Post, Comment
 
+
 class PostListView(ListView):
     model = Post
+
 
 class PostCreateView(CreateView):
     model = Post
     fields = ['title', 'content', 'categories',]
     success_url = reverse_lazy('posts')
-    
+
     def form_valid(self, form):
-            form.instance.owner = self.request.user
-            return super().form_valid(form)
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'content', 'categories',]
+    success_url = reverse_lazy('posts')
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = reverse_lazy('posts')
+    template_name = 'blog/post_confirm_delete.html'
